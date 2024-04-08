a7099dab9a131a933ce2d794d4831d9b1697a5be
WebCoder <destinyprogrammer06@gmail.com>
2024-04-08 18:08:38 +0100

    last attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import { Fragment, useState } from 'react';
 import NavBar from './components/NavBar';
 import image1 from './images/image-product-1.jpg';
+import image2 from './images/image-product-2.jpg';
+import image3 from './images/image-product-3.jpg';
+import image4 from './images/image-product-4.jpg';
+import image5 from './images/image-product-5.jpg';
 
 import './App.css';
 import ImageCarousel from './components/ImageCarousel';
 import Main from './components/Main';
 import Cart from './components/Cart';
 
-const slides = [
-  image1,
-  '../src/images/image-product-2.jpg',
-  './src/images/image-product-3.jpg',
-  '/src/images/image-product-4.jpg',
-  '/images/image-product-1.jpg',
-  '../images/image-product-1.jpg',
-];
+const slides = [image1, image2, image3, image4, './images/image-product-5.jpg'];
 
 function App() {
   const [totalItem, setTotalItem] = useState(0);
